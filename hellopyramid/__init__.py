c6df82a04e757ee83990ed7826fd37d2202a7530
Billy Tat <billyt@activestate.com>
2014-08-06 13:17:26 -0700

    Revert 55fd38e04b15c93733f43fda04af22ebc27245a0 and make copy of mytemplate.pt that uses https for source of assets then use when Stackato sso is enabled

--- hellopyramid/__init__.py
+++ hellopyramid/__init__.py
@@ -1,14 +1,21 @@
 from pyramid.config import Configurator
 from hellopyramid.resources import Root
+import os
+import json
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
+    sso_enabled = json.loads(os.environ['VCAP_APPLICATION'])['sso_enabled']
+    if sso_enabled:
+        template = 'hellopyramid:templates/mytemplate_https.pt'
+    else:
+        template = 'hellopyramid:templates/mytemplate.pt'
     config = Configurator(root_factory=Root, settings=settings)
     config.include('pyramid_chameleon')
     config.add_view('hellopyramid.views.my_view',
                     context='hellopyramid:resources.Root',
-                    renderer='hellopyramid:templates/mytemplate.pt')
+                    renderer=template)
     config.add_static_view('static', 'hellopyramid:static')
     return config.make_wsgi_app()
 
