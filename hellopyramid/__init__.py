04cadb8a53cef4ed752116524a2a63d65accf2c2
Billy Tat <billy.tat@hpe.com>
2016-09-19 15:11:40 -0700

    Update for Stackato 4.0

--- hellopyramid/__init__.py
+++ hellopyramid/__init__.py
@@ -6,11 +6,7 @@ import json
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
-    sso_enabled = json.loads(os.environ['VCAP_APPLICATION'])['sso_enabled']
-    if sso_enabled:
-        template = 'hellopyramid:templates/mytemplate_https.pt'
-    else:
-        template = 'hellopyramid:templates/mytemplate.pt'
+    template = 'hellopyramid:templates/mytemplate.pt'
     config = Configurator(root_factory=Root, settings=settings)
     config.include('pyramid_chameleon')
     config.add_view('hellopyramid.views.my_view',
